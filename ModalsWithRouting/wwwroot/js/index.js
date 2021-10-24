5fd85fa9bf65a895ef3fa7ee14817db1d57893e1
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2021-10-24 13:01:50 +0200

    Add project files.

---
+++ ModalsWithRouting/wwwroot/js/index.js
@@ -0,0 +1,10 @@
+
+ 
+// if(window.location.pathname !== '/index.html')
+// {
+//     let iframe = document.createElement('iframe');
+//     iframe.src = window.location;
+//     iframe.className = 'modal-iframe noselect';
+//     window.location.href = '/index.html';
+//     document.body.appendChild(iframe);
+// }
